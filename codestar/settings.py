efb621b281660c785b2d2a26c7c1f469e2e58de8
Joseph Chan <joschan@yahoo.co.uk>
2024-07-10 18:16:35 +0000

    Correct code in allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,8 +30,10 @@ SECRET_KEY = 'django-insecure-&nvagev!ikub((a2w0ync=ze#8mre55^e610(@uaamn3k)2x%y
 #DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-joschanuk-djangoblog-mc3dx5jqz0a.ws.codeinstitute-ide.net',
-                '.herokuapp.com']
+ALLOWED_HOSTS = [
+    '8000-joschanuk-djangoblog-mc3dx5jqz0a.ws.codeinstitute-ide.net',
+    '.herokuapp.com',
+]
 
 
 # Application definition
